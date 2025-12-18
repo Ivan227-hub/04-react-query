51dfb81bdd413b416a66829ea1ba09be1733319d
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-12-18 18:57:30 +0200

    bugs 2

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
-import type { MoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
+
+interface MoviesResponse {
+  results: Movie[];
+  total_pages: number;
+}
+
 
 const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWZkNDUxMWQ3OGQzZTg1N2IyOGMxM2M4YzQ1N2NiOSIsIm5iZiI6MTc2NDY3Nzk4OS42NDksInN1YiI6IjY5MmVkOTY1NDVjZmU4NGUxNjI2ZjNkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kudrzA3OazjiYVuWZnmFvrkra4T0eI3InwXrJbmXy4Y"; 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
