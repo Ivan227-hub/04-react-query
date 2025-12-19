51c4a6cf453f476995f6b41eaf57adaf839387e0
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-12-19 13:33:17 +0200

    fixed bugs

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,12 +1,9 @@
-// types/movie.ts
-
 export interface Movie {
   id: number;
   title: string;
+  overview: string;
   poster_path: string | null;
   backdrop_path: string | null;
-  overview: string;
   release_date: string;
   vote_average: number;
 }
-
